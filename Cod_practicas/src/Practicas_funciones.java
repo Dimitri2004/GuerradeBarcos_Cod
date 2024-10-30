325dd4dc7419892a5a824eaceb80e161ac449449
Dima <dapariciopereira@danielcastelao.org>
2024-10-30 14:30:27 +0100

    Boletin 6 con ejercicios corregidos 1.0

---
+++ Cod_practicas/src/Practicas_funciones.java
@@ -0,0 +1,34 @@
+/**
+ * @author Dima Aparicio
+ * @version 1.0
+ */
+public class Practicas_funciones {
+
+
+    /**
+     * Entrada principal en la aplicacion
+     *
+     * @param args no usados
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Cuenta la totalidad de las <strong>letras de los caracteres </strong>
+     *
+     * @param palabra_uno palabra a contar caracteres
+     * @param palabra_dos palabra a contar caracteres
+     * @return numero de letras totales
+     * @since 1.0
+     */
+    static int cuenta_letras(String palabra_uno, String palabra_dos) {
+
+        int totales = palabra_uno.length() + palabra_dos.length();
+
+        return 0;
+
+    }
+
+
+}
